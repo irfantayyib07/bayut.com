5066d8449c7fb9a24d42aa30f37e7889ad9e25fb
Irfan <irfantayyib07@gmail.com>
2024-03-16 16:35:29 +0500

    DD done

--- build/script.js
+++ build/script.js
@@ -17,32 +17,34 @@ RADIO_BUTTONS_CONTAINER.addEventListener("click", (e) => {
 const SELECT_FIRST_TABS_CONTAINER = document.getElementById("SELECT_FIRST_TABS_CONTAINER");
 const SELECT_FIRST_TAB_OPTIONS_CONTAINER = document.getElementById("SELECT_FIRST_TAB_OPTIONS_CONTAINER");
 
+SELECT_FIRST_TABS_CONTAINER.addEventListener("click", (e) => {
+ if (e.target.tagName !== "BUTTON") return;
+ for (let elem of SELECT_FIRST_TABS_CONTAINER.children) elem.classList.remove("selected-tab");
+ e.target.classList.add("selected-tab"); // add the "selected" style to the tab which is clicked
+
+ for (let elem of SELECT_FIRST_TAB_OPTIONS_CONTAINER.children) {
+  elem.classList.add("w-0", "h-0", "invisible", "absolute"); // remove all tab options
+
+  if (elem.id === e.target.dataset.controls) {
+   elem.classList.remove("w-0", "h-0", "invisible", "absolute"); // show those options that match the tab which has been clicked
+  }
+ }
+});
+
+// the loop below removes (visually) other tab options in the dropdown, while keeping the options for the first tab (runs every time a page reload, it is not an event handler), making the first tab to be selected by default
+
 for (let elem of SELECT_FIRST_TAB_OPTIONS_CONTAINER.children) {
  if (elem.id !== SELECT_FIRST_TABS_CONTAINER.children["SELECT_FIRST_CATEGORY1_TAB"].dataset.controls)
   elem.classList.add("w-0", "h-0", "invisible", "absolute");
 }
 
-SELECT_FIRST_TABS_CONTAINER.addEventListener("click", (e) => {
- if (e.target.tagName !== "BUTTON") return;
- for (let elem of e.target.parentElement.children) elem.classList.remove("selected-tab");
- e.target.classList.add("selected-tab");
-
- for (let elem of SELECT_FIRST_TAB_OPTIONS_CONTAINER.children) {
-  elem.classList.add("w-0", "h-0", "invisible", "absolute");
-
-  if (elem.id === e.target.dataset.controls) {
-   elem.classList.remove("w-0", "h-0", "invisible", "absolute");
-  }
- }
-});
-
 // DD TABS LOGIC ENDS HERE
 
-const SELECT_FIRST_LABEL = document.getElementById("SELECT_FIRST_LABEL");
-const SELECT_FIRST_DROPDOWN = document.getElementById("SELECT_FIRST_DROPDOWN");
-const SELECT_FIRST = document.getElementById("SELECT_FIRST");
 const SELECT_TOGGLER = document.getElementById("SELECT_TOGGLER");
+const SELECT_FIRST_LABEL = document.getElementById("SELECT_FIRST_LABEL");
 const SELECT_FIRST_CHEVRON = document.getElementById("SELECT_FIRST_CHEVRON");
+const SELECT_FIRST_DROPDOWN = document.getElementById("SELECT_FIRST_DROPDOWN");
+const SELECT_FIRST = document.getElementById("SELECT_FIRST"); // <select> element to send the data to the server
 
 SELECT_FIRST.options[0].selected = true; // to reset the value of select to empty string upon page reload
 
@@ -62,8 +64,8 @@ SELECT_FIRST_DROPDOWN.addEventListener("click", (e) => {
   }
  }
 
- if (e.target.id === "SELECT_FIRST_DD_DONE") SELECT_TOGGLER.checked = false;
- if (e.target.id === "SELECT_FIRST_DD_RESET") {
+ if (e.target.id === "SELECT_FIRST_DD_DONE_BUTTON") SELECT_TOGGLER.checked = false;
+ if (e.target.id === "SELECT_FIRST_DD_RESET_BUTTON") {
   SELECT_FIRST.value = null;
   SELECT_FIRST_LABEL.childNodes[0].replaceWith("Category1");
   SELECT_TOGGLER.checked = false;
