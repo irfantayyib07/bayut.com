6176e1582d8dd5d3833323d90e07252152ca3dfb
Irfan <irfantayyib07@gmail.com>
2024-03-28 22:16:44 +0500

    done

--- build/script.js
+++ build/script.js
@@ -1,6 +1,31 @@
 "use strict";
 
 const FILTER_FORM = document.getElementById("filter-form");
+const OWNERSHIP_OPTIONS_CONTAINER = document.getElementById("ownership-options-container");
+const CATEGORY_SELECT_TABS_CONTAINER = document.getElementById("category-select-tabs-container");
+const CATEGORY_SELECT_OPTIONS_CONTAINER = document.getElementById("category-select-options-container");
+const CATEGORY_SELECT_TOGGLER = document.getElementById("category-select-toggler");
+const CATEGORY_SELECT_LABEL = document.getElementById("category-select-label");
+const CATEGORY_SELECT_DROPDOWN = document.getElementById("category-select-dropdown");
+const CATEGORY_SELECT = document.getElementById("category-select"); // <select> element to send the data to the server
+const BEDSBATHS_SELECT_TOGGLER = document.getElementById("bedsbaths-select-toggler");
+const BEDSBATHS_SELECT_LABEL = document.getElementById("bedsbaths-select-label");
+const BEDSBATHS_SELECT_ARROW_UP = document.getElementById("bedsbaths-select-arrow-up");
+const BEDSBATHS_SELECT_DROPDOWN = document.getElementById("bedsbaths-select-dropdown");
+const BEDROOMS_OPTIONS_CONTAINER = document.getElementById("bedrooms-options-container");
+const BATHROOMS_OPTIONS_CONTAINER = document.getElementById("bathrooms-options-container");
+const AMOUNT_SELECT_TOGGLER = document.getElementById("amount-select-toggler");
+const AMOUNT_SELECT_LABEL = document.getElementById("amount-select-label");
+const AMOUNT_SELECT_ARROW_UP = document.getElementById("amount-select-arrow-up");
+const AMOUNT_SELECT_DROPDOWN = document.getElementById("amount-select-dropdown");
+const PRICE_FILTER_CONTAINER = document.getElementById("price-filter-container");
+const MINIMUM_FIELD = document.getElementById("minimum");
+const MAXIMUM_FIELD = document.getElementById("maximum");
+const MINIMUM_FIELD_CONTAINER = document.getElementById("minimum-field-container");
+const MAXIMUM_FIELD_CONTAINER = document.getElementById("maximum-field-container");
+const CATEGORY_SELECT_CHEVRON = document.getElementById("category-select-chevron");
+const BEDSBATHS_SELECT_CHEVRON = document.getElementById("bedsbaths-select-chevron");
+const AMOUNT_SELECT_CHEVRON = document.getElementById("amount-select-chevron");
 
 document.body.onload = () => {
  FILTER_FORM.reset();
@@ -9,20 +34,15 @@ document.body.onload = () => {
 
 // RADIO BUTTONS
 
-const OWNERSHIP_OPTIONS_CONTAINER = document.getElementById("ownership-options-container");
-
 OWNERSHIP_OPTIONS_CONTAINER.addEventListener("click", (e) => {
  if (e.target.tagName !== "LABEL") return;
  for (let elem of OWNERSHIP_OPTIONS_CONTAINER.children) elem.classList.remove("tw-checked-radio-label");
  e.target.classList.add("tw-checked-radio-label");
 });
 
-// category-select DD (DROPDOWN)
+// CATEGORY SELECT DD (DROPDOWN)
 
 // DD TABS
-
-const CATEGORY_SELECT_TABS_CONTAINER = document.getElementById("category-select-tabs-container");
-const CATEGORY_SELECT_OPTIONS_CONTAINER = document.getElementById("category-select-options-container");
 
 CATEGORY_SELECT_TABS_CONTAINER.addEventListener("click", (e) => {
  if (e.target.tagName !== "BUTTON") return;
@@ -47,12 +67,6 @@ for (let elem of CATEGORY_SELECT_OPTIONS_CONTAINER.children) {
 
 // DD TABS LOGIC ENDS HERE
 
-const CATEGORY_SELECT_TOGGLER = document.getElementById("category-select-toggler");
-const CATEGORY_SELECT_LABEL = document.getElementById("category-select-label");
-const CATEGORY_SELECT_CHEVRON = document.getElementById("category-select-chevron");
-const CATEGORY_SELECT_DROPDOWN = document.getElementById("category-select-dropdown");
-const CATEGORY_SELECT = document.getElementById("category-select"); // <select> element to send the data to the server
-
 CATEGORY_SELECT.options[0].selected = true; // to reset the value of select to empty string upon page reload
 // const height = CATEGORY_SELECT_DROPDOWN.offsetHeight + 12;
 // const width = height * 1.75;
@@ -61,16 +75,9 @@ CATEGORY_SELECT_DROPDOWN.addEventListener("click", (e) => {
  if (e.target.closest("#category-select-options-container") && e.target.tagName === "SPAN") {
   // CATEGORY_SELECT_TOGGLER.checked = false;
   CATEGORY_SELECT_LABEL.childNodes[0].textContent = e.target.textContent;
-  for (let option of CATEGORY_SELECT.options) {
-   if (option.textContent.toLowercase() === e.target.textContent.toLowercase()) option.selected = true;
-  }
-
-  for (let div of CATEGORY_SELECT_OPTIONS_CONTAINER.children) {
-   for (let span of div.children) {
-    span.classList.remove("tw-selected-option");
-    if (span === e.target) e.target.classList.add("tw-selected-option");
-   }
-  }
+  CATEGORY_SELECT.querySelector(`option[value="${e.target.textContent.toLowerCase()}"]`).selected = true;
+  CATEGORY_SELECT_OPTIONS_CONTAINER.querySelector(".tw-selected-option")?.classList.remove("tw-selected-option");
+  e.target.classList.add("tw-selected-option");
  }
 
  if (e.target.id === "category-select-done-btn") CATEGORY_SELECT_TOGGLER.checked = false;
@@ -79,25 +86,13 @@ CATEGORY_SELECT_DROPDOWN.addEventListener("click", (e) => {
   CATEGORY_SELECT_LABEL.childNodes[0].textContent = "Category";
   // CATEGORY_SELECT_TOGGLER.checked = false;
 
-  for (let options of CATEGORY_SELECT_OPTIONS_CONTAINER.children) {
-   for (let option of options.children) {
-    option.classList.remove("tw-selected-option");
-   }
-  }
+  CATEGORY_SELECT_OPTIONS_CONTAINER.querySelector(".tw-selected-option")?.classList.remove("tw-selected-option");
 
   CATEGORY_SELECT_TABS_CONTAINER.children["category-select-residential-tab"].click();
  }
 });
 
-// SELECT_SECOND DD (DROPDOWN)
-
-const BEDSBATHS_SELECT_TOGGLER = document.getElementById("bedsbaths-select-toggler");
-const BEDSBATHS_SELECT_LABEL = document.getElementById("bedsbaths-select-label");
-const BEDSBATHS_SELECT_CHEVRON = document.getElementById("bedsbaths-select-chevron");
-const BEDSBATHS_SELECT_ARROW_UP = document.getElementById("bedsbaths-select-arrow-up");
-const BEDSBATHS_SELECT_DROPDOWN = document.getElementById("bedsbaths-select-dropdown");
-const BEDROOMS_OPTIONS_CONTAINER = document.getElementById("bedrooms-options-container");
-const BATHROOMS_OPTIONS_CONTAINER = document.getElementById("bathrooms-options-container");
+// BEDROOMS AND BATHROOMS DD (DROPDOWN)
 
 BEDSBATHS_SELECT_DROPDOWN.addEventListener("click", (e) => {
  if (
@@ -147,19 +142,7 @@ BEDSBATHS_SELECT_DROPDOWN.addEventListener("click", (e) => {
  }
 });
 
-// SELECT_THIRD DD (DROPDOWN)
-
-const SELECT_THIRD_TOGGLER = document.getElementById("SELECT_THIRD_TOGGLER");
-const SELECT_THIRD_LABEL = document.getElementById("SELECT_THIRD_LABEL");
-const SELECT_THIRD_CHEVRON = document.getElementById("SELECT_THIRD_CHEVRON");
-const SELECT_THIRD_ARROW_UP = document.getElementById("SELECT_THIRD_ARROW_UP");
-const SELECT_THIRD_DROPDOWN = document.getElementById("SELECT_THIRD_DROPDOWN");
-
-const PRICE_FILTER_CONTAINER = document.getElementById("PRICE_FILTER_CONTAINER");
-const MINIMUM_FIELD = document.getElementById("minimum");
-const MAXIMUM_FIELD = document.getElementById("maximum");
-const MINIMUM_FIELD_CONTAINER = document.getElementById("MINIMUM_FIELD_CONTAINER");
-const MAXIMUM_FIELD_CONTAINER = document.getElementById("MAXIMUM_FIELD_CONTAINER");
+// AMOUNT SELECTOR DD (DROPDOWN)
 
 const options = ["10,000", "20,000", "30,000", "40,000", "50,000", "60,000"];
 let nestedDropdown = document.createElement("div");
@@ -180,17 +163,17 @@ MAXIMUM_FIELD.addEventListener("focus", (e) => {
  MAXIMUM_FIELD_CONTAINER.append(nestedDropdown);
 });
 
-SELECT_THIRD_DROPDOWN.addEventListener("click", (e) => {
- if (e.target.closest("#SELECT_THIRD_CONTAINER") && e.target.tagName !== "INPUT") {
+AMOUNT_SELECT_DROPDOWN.addEventListener("click", (e) => {
+ if (e.target.closest("#amount-select-container") && e.target.tagName !== "INPUT") {
   nestedDropdown.remove();
  }
 
- if (e.target.id === "SELECT_THIRD_DD_DONE_BUTTON") SELECT_THIRD_TOGGLER.checked = false;
- if (e.target.id === "SELECT_THIRD_DD_RESET_BUTTON") {
-  SELECT_THIRD_LABEL.childNodes[0].textContent = "Price (AED)";
+ if (e.target.id === "amount-select-done-btn") AMOUNT_SELECT_TOGGLER.checked = false;
+ if (e.target.id === "amount-select-reset-btn") {
+  AMOUNT_SELECT_LABEL.childNodes[0].textContent = "Price (AED)";
   MINIMUM_FIELD.value = 0;
   MAXIMUM_FIELD.value = "";
-  // SELECT_THIRD_TOGGLER.checked = false;
+  // AMOUNT_SELECT_TOGGLER.checked = false;
  }
 });
 
@@ -221,7 +204,7 @@ PRICE_FILTER_CONTAINER.addEventListener("change", (e) => {
   parseInt(MAXIMUM_FIELD.value) === 0 || MAXIMUM_FIELD.value === ""
    ? "Any"
    : MAXIMUM_FIELD.value / 1000 + "K";
- SELECT_THIRD_LABEL.childNodes[0].textContent = `AED ${minAmount} - ${maxAmount}`;
+ AMOUNT_SELECT_LABEL.childNodes[0].textContent = `AED ${minAmount} - ${maxAmount}`;
 });
 
 // DD CLOSE-WHEN-CLICKED-OUTSIDE LOGIC
@@ -231,14 +214,9 @@ document.addEventListener("click", (e) => {
 
  if (CATEGORY_SELECT_TOGGLER.checked) {
   CATEGORY_SELECT_DROPDOWN.classList.remove("tw-invisible");
-  CATEGORY_SELECT_DROPDOWN.style.paddingBottom = "12px";
-  // CATEGORY_SELECT_DROPDOWN.style.maxHeight = height + "px";
-  // CATEGORY_SELECT_DROPDOWN.style.width = width + "px";
   CATEGORY_SELECT_CHEVRON.style.transform = "rotate(180deg)";
  } else {
   CATEGORY_SELECT_DROPDOWN.classList.add("tw-invisible");
-  // CATEGORY_SELECT_DROPDOWN.style.paddingBottom = "";
-  // CATEGORY_SELECT_DROPDOWN.style.maxHeight = 0;
   CATEGORY_SELECT_CHEVRON.style.transform = "rotateY(360deg)";
  }
 
@@ -246,34 +224,23 @@ document.addEventListener("click", (e) => {
 
  if (BEDSBATHS_SELECT_TOGGLER.checked) {
   BEDSBATHS_SELECT_DROPDOWN.classList.remove("tw-invisible");
-  BEDSBATHS_SELECT_DROPDOWN.style.paddingBottom = "12px";
-  // BEDSBATHS_SELECT_DROPDOWN.style.maxHeight = height + "px";
-  // BEDSBATHS_SELECT_DROPDOWN.style.width = width + "px";
   BEDSBATHS_SELECT_CHEVRON.style.transform = "rotate(180deg)";
  } else {
   BEDSBATHS_SELECT_DROPDOWN.classList.add("tw-invisible");
-  // BEDSBATHS_SELECT_DROPDOWN.style.paddingBottom = "";
-  // BEDSBATHS_SELECT_DROPDOWN.style.maxHeight = 0;
   BEDSBATHS_SELECT_CHEVRON.style.transform = "rotateY(360deg)";
  }
 
- console.log(`Range ${MINIMUM_FIELD.value} - ${MAXIMUM_FIELD.value}`);
- if (!e.target.closest("#SELECT_THIRD_CONTAINER") && !e.target.closest(".tw-nested-dropdown-style")) {
-  SELECT_THIRD_TOGGLER.checked = false;
+ if (!e.target.closest("#amount-select-container") && !e.target.closest(".tw-nested-dropdown-style")) {
+  AMOUNT_SELECT_TOGGLER.checked = false;
   nestedDropdown.remove();
  }
 
- if (SELECT_THIRD_TOGGLER.checked) {
-  SELECT_THIRD_DROPDOWN.classList.remove("tw-invisible");
-  SELECT_THIRD_DROPDOWN.style.paddingBottom = "12px";
-  // SELECT_THIRD_DROPDOWN.style.maxHeight = height + "px";
-  // SELECT_THIRD_DROPDOWN.style.width = width + "px";
-  SELECT_THIRD_CHEVRON.style.transform = "rotate(180deg)";
+ if (AMOUNT_SELECT_TOGGLER.checked) {
+  AMOUNT_SELECT_DROPDOWN.classList.remove("tw-invisible");
+  AMOUNT_SELECT_CHEVRON.style.transform = "rotate(180deg)";
  } else {
-  SELECT_THIRD_DROPDOWN.classList.add("tw-invisible");
-  // SELECT_THIRD_DROPDOWN.style.paddingBottom = "";
-  // SELECT_THIRD_DROPDOWN.style.maxHeight = 0;
-  SELECT_THIRD_CHEVRON.style.transform = "rotateY(360deg)";
+  AMOUNT_SELECT_DROPDOWN.classList.add("tw-invisible");
+  AMOUNT_SELECT_CHEVRON.style.transform = "rotateY(360deg)";
  }
 });
 
@@ -303,10 +270,12 @@ for (let dropdown of document.querySelectorAll(".tw-dropdown")) {
 }
 
 window.addEventListener("resize", function (e) {
- flipDropDown(document.querySelector(".tw-dropdown:not(.tw-invisible)"));
+ for (let dropdown of document.querySelectorAll(".tw-dropdown")) {
+  flipDropDown(dropdown);
+ }
 });
 
 window.onerror = (msg, url, line, col, error) => {
- const extra = (!col ? '' : `\ncolumn: ${col}`) + (!error ? '' : `\nerror: ${error}`);
- console.log(`Error: ${msg}\nurl: ${url}\nline: ${line}${extra}`);
+ console.log(error);
+ return true;
 };
