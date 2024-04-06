94ee6af9403c2de0d7cbf481d8531f83fa091953
Irfan <irfantayyib07@gmail.com>
2024-04-06 22:22:13 +0500

    NS

--- build/script.js
+++ build/script.js
@@ -1,9 +1,5 @@
 "use strict";
 
-function removeExtraWhitespace(inputString) {
- return inputString.replace(/[\s\n]+/g, ' ').trim();
-}
-
 const FILTER_FORM = document.getElementById("filter-form");
 const OWNERSHIP_OPTIONS_CONTAINER = document.getElementById("ownership-options-container");
 const CATEGORY_SELECT_TABS_CONTAINER = document.getElementById("category-select-tabs-container");
@@ -80,7 +76,7 @@ CATEGORY_SELECT_DROPDOWN.addEventListener("click", (e) => {
   // CATEGORY_SELECT_TOGGLER.checked = false;
   CATEGORY_SELECT_LABEL.childNodes[0].textContent = e.target.textContent;
   console.log(e.target.textContent.toLowerCase());
-  CATEGORY_SELECT.querySelector(`option[value="${removeExtraWhitespace(e.target.textContent.toLowerCase())}"]`).selected = true;
+  CATEGORY_SELECT.querySelector(`option[value="${e.target.textContent.toLowerCase().replace(/[\s\n]+/g, ' ').trim()}"]`).selected = true;
   for (let span of CATEGORY_SELECT_OPTIONS_CONTAINER.querySelectorAll("span")) {
    span.classList.remove("tw-selected-option");
    span.classList.add("hover:tw-bg-gray-100");
